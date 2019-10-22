2e2f3de8a03a6ab9fda465b61408c9813c56d8ee
kameron-mcadams <56734925+kameron-mcadams@users.noreply.github.com>
2019-10-22 13:12:34 -0500

    Add files via upload

---
+++ Lab02.py
@@ -0,0 +1,15 @@
+pennies = int(input("How many pennies do you have?"))
+nickels = int(input("How many nickels do you have?"))
+dimes = int(input("How many dimes do you have?"))
+quarters = int(input("How many quarters do you have?"))
+halfDollars = int(input("How many half dollars do you have?"))
+dollars = int(input("How many dollars do you have?"))
+numPennies = pennies * 1
+numNickels = nickels * 5
+numDimes = dimes * 10
+numQuarters = quarters * 25
+numHalfDollars = halfDollars * 50
+numDollars = dollars * 100
+total = numPennies + numNickels + numDimes + numQuarters + numHalfDollars + numDollars
+money = total / 100
+print("You have $" ,money, ".")
